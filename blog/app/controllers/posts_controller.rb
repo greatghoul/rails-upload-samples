5db416e24e86a0f3edb5291b462787ff42d0b11d
greatghoul <greatghoul@gmail.com>
2014-05-16 22:20:01 +0800

    实现伪异步上传

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
+        format.html
         format.json { render action: 'show', status: :created, location: @post }
       else
         format.html { render action: 'index' }
